7cf0d808a4ee24238f2d9cb34ab46d55407ab2b8
splushechka <svitenkosofia1@gmail.com>
2024-06-22 23:21:54 +0300

    розділення мейнлупа на методи

--- boooooom/Game/GameProcess.cs
+++ boooooom/Game/GameProcess.cs
@@ -6,66 +6,69 @@ using boooooom.Enums;
 using boooooom.Non_Entity_Classes;
 using Action = boooooom.NonEntityClasses.Action;
 
-namespace boooooom.Game;
-
-public class GameProcess
+namespace boooooom.Game
 {
-    private readonly int WinningScoreThreshold;
-    
-    private int loopCount = 0;
-    
-    private Coordinates playerStartCoords;
-    public int Score { get; set; }
-    
-    public Cell[,] Field { get; set; }
-
-    public Coordinates PlayerCoords { get; set; }
-    
-    public Action? Action { get; set; }
-    
-    public List<Enemy> Enemies { get; set; }
-    
-    public GameTimer Timer { get; set; }
-    
-    public bool IsGameOver { get; set; }
-    
-    public bool Win { get; set; }
-
-    public GameProcess(Coordinates playerCoords, int winningScoreThreshold, List<Enemy> enemies)
+    public class GameProcess
     {
-        PlayerCoords = playerCoords;
-        playerStartCoords = playerCoords;
-        Timer = new GameTimer(MainLoop);
-        Timer.StartTimer();
-        WinningScoreThreshold = winningScoreThreshold;
-        Enemies = enemies;
-    }
-    
-    private void RefreshEnemies()
-    {
-        Enemies.Clear();
-        for (int y = 0; y < Field.GetLength(0); y++)
+        private readonly int WinningScoreThreshold;
+        private int loopCount = 0;
+        private Coordinates playerStartCoords;
+        public int Score { get; set; }
+        public Cell[,] Field { get; set; }
+        public Coordinates PlayerCoords { get; set; }
+        public Action? Action { get; set; }
+        public List<Enemy> Enemies { get; set; }
+        public GameTimer Timer { get; set; }
+        public bool IsGameOver { get; set; }
+        public bool Win { get; set; }
+
+        public GameProcess(Coordinates playerCoords, int winningScoreThreshold, List<Enemy> enemies)
         {
-            for (int x = 0; x < Field.GetLength(1); x++)
+            PlayerCoords = playerCoords;
+            playerStartCoords = playerCoords;
+            Timer = new GameTimer(MainLoop);
+            Timer.StartTimer();
+            WinningScoreThreshold = winningScoreThreshold;
+            Enemies = enemies;
+        }
+
+        private void RefreshEnemies()
+        {
+            Enemies.Clear();
+            for (int y = 0; y < Field.GetLength(0); y++)
             {
-                var cell = Field[y, x];
-                foreach (var entity in cell.EntitiesOnCell)
+                for (int x = 0; x < Field.GetLength(1); x++)
                 {
-                    if (entity is Enemy enemy)
+                    var cell = Field[y, x];
+                    foreach (var entity in cell.EntitiesOnCell)
                     {
-                        Enemies.Add(enemy);
+                        if (entity is Enemy enemy)
+                        {
+                            Enemies.Add(enemy);
+                        }
                     }
                 }
             }
         }
-    }
-    
-    public void MainLoop(object sender, ElapsedEventArgs e)
-    {
-        var changedCells = new List<(Coordinates coords, Cell cell)>();
-        loopCount++;
 
-        if (loopCount % 2 == 0)
+        public void MainLoop(object sender, ElapsedEventArgs e)
+        {
+            var changedCells = new List<(Coordinates coords, Cell cell)>();
+            loopCount++;
+
+            if (loopCount % 2 == 0)
+            {
+                MoveEnemies(changedCells);
+                CheckPlayerCollisions(changedCells);
+                GameRender.DrawChanges(changedCells);
+            }
+
+            HandlePlayerAction(changedCells);
+            HandleExplosions(changedCells);
+            GameRender.DrawChanges(changedCells);
+        }
+
+        private void MoveEnemies(List<(Coordinates coords, Cell cell)> changedCells)
         {
             foreach (var enemy in Enemies)
             {
@@ -74,30 +77,29 @@ public class GameProcess
                 var moveChange = enemy.Move();
                 var newCoords = new Coordinates(currentCoords.X + moveChange.X, currentCoords.Y + moveChange.Y);
 
-                // Перевірка меж поля
                 if (newCoords.X >= 0 && newCoords.X < Field.GetLength(1) && newCoords.Y >= 0 && newCoords.Y < Field.GetLength(0))
                 {
                     var newCell = Field[newCoords.Y, newCoords.X];
 
-                    // Перевірка прохідності нової клітинки
                     if (newCell.CanActiveEntityStep())
                     {
                         currentCell.EntitiesOnCell.Remove(enemy);
                         newCell.EntitiesOnCell.Add(enemy);
                         enemy.CurrentCoords = newCoords;
-      
+
                         changedCells.Add((currentCoords, currentCell));
                         changedCells.Add((newCoords, newCell));
                     }
                     else if (enemy is LinearEnemy linearEnemy)
                     {
                         linearEnemy.ReverseDirection();
-
                     }
                 }
             }
+        }
 
-            // Перевірка на зіткнення з гравцем
+        private void CheckPlayerCollisions(List<(Coordinates coords, Cell cell)> changedCells)
+        {
             foreach (var enemy in Enemies)
             {
                 var playerCoords = PlayerCoords;
@@ -121,8 +123,6 @@ public class GameProcess
                     PlayerCoords = playerStartCoords;
 
                     GameRender.DrawLives(player.Lives, Field.GetLength(0));
-                    GameRender.DrawChanges(changedCells);
-
                     if (player.IsDead())
                     {
                         Timer.StopTimer();
@@ -130,110 +130,117 @@ public class GameProcess
                     }
                     else
                     {
-                        GameRender.DrawChanges(changedCells);
                         GameRender.DrawLives(player.Lives, Field.GetLength(0));
                     }
                 }
             }
-
-            GameRender.DrawChanges(changedCells);
         }
 
-        // Логіка реакції геймпроцесу на дії гравця
-        if (Action != null)
+        private void HandlePlayerAction(List<(Coordinates coords, Cell cell)> changedCells)
         {
-            var currentCell = Field[PlayerCoords.Y, PlayerCoords.X];
-            if (Action.Type == ActionType.Move)
+            if (Action != null)
             {
-                var newCoords = new Coordinates(PlayerCoords.X + Action.CoordinatesChange.X, PlayerCoords.Y + Action.CoordinatesChange.Y);
-
-                if (newCoords.X >= 0 && newCoords.X < Field.GetLength(1) && newCoords.Y >= 0 && newCoords.Y < Field.GetLength(0))
+                var currentCell = Field[PlayerCoords.Y, PlayerCoords.X];
+                if (Action.Type == ActionType.Move)
                 {
-                    var newCell = Field[newCoords.Y, newCoords.X];
-                    if (newCell.CanActiveEntityStep())
+                    var newCoords = new Coordinates(PlayerCoords.X + Action.CoordinatesChange.X, PlayerCoords.Y + Action.CoordinatesChange.Y);
+
+                    if (newCoords.X >= 0 && newCoords.X < Field.GetLength(1) && newCoords.Y >= 0 && newCoords.Y < Field.GetLength(0))
                     {
-                        var entity = currentCell.EntitiesOnCell.Select((x, idx) => (idx, x)).FirstOrDefault(pair => pair.x.GetEntityType() == ActiveEntityType.Player);
-                        newCell.EntitiesOnCell.Add(entity.x);
-                        currentCell.EntitiesOnCell.RemoveAt(entity.idx);
-                        changedCells.Add((PlayerCoords, currentCell));
-                        PlayerCoords = newCoords;
-                        changedCells.Add((newCoords, newCell));
-
-                        if (newCell.PrizeOnCell is Heart)
+                        var newCell = Field[newCoords.Y, newCoords.X];
+                        if (newCell.CanActiveEntityStep())
                         {
-                            var player = entity.x as PlayerEntity;
-                            player.Lives += 1;
-                            newCell.PrizeOnCell = null;
-                            GameRender.DrawLives(player.Lives, Field.GetLength(0));
-                        }
+                            var entity = currentCell.EntitiesOnCell.Select((x, idx) => (idx, x)).FirstOrDefault(pair => pair.x.GetEntityType() == ActiveEntityType.Player);
+                            newCell.EntitiesOnCell.Add(entity.x);
+                            currentCell.EntitiesOnCell.RemoveAt(entity.idx);
+                            changedCells.Add((PlayerCoords, currentCell));
+                            PlayerCoords = newCoords;
+                            changedCells.Add((newCoords, newCell));
 
-                        if (currentCell.HasPrize(out _))
-                        {
-                            currentCell.PrizeOnCell = null;
-                        }
-
-                        if (newCell.HasPrize(out var prizeValue))
-                        {
-                            Score += prizeValue;
-                            newCell.PrizeOnCell.Collect();
-                            GameRender.DrawScore(Score, Field.GetLength(0));
-
-                            if (Score >= WinningScoreThreshold)
+                            if (newCell.PrizeOnCell is Heart)
                             {
-                                HandleWin();
+                                var player = entity.x as PlayerEntity;
+                                player.Lives += 1;
+                                newCell.PrizeOnCell = null;
+                                GameRender.DrawLives(player.Lives, Field.GetLength(0));
+                            }
+
+                            if (currentCell.HasPrize(out _))
+                            {
+                                currentCell.PrizeOnCell = null;
+                            }
+
+                            if (newCell.HasPrize(out var prizeValue))
+                            {
+                                Score += prizeValue;
+                                newCell.PrizeOnCell.Collect();
+                                GameRender.DrawScore(Score, Field.GetLength(0));
+
+                                if (Score >= WinningScoreThreshold)
+                                {
+                                    HandleWin();
+                                }
                             }
                         }
                     }
                 }
-            }
-            else if (Action.Type == ActionType.PlaceBomb)
-            {
-                if (currentCell.BombOnCell == null)
+                else if (Action.Type == ActionType.PlaceBomb)
                 {
-                    if (currentCell is EmptyCell emptyCell)
+                    if (currentCell.BombOnCell == null)
                     {
-                        emptyCell.PlaceBomb();
-                        changedCells.Add((PlayerCoords, currentCell));
+                        if (currentCell is EmptyCell emptyCell)
+                        {
+                            emptyCell.PlaceBomb();
+                            changedCells.Add((PlayerCoords, currentCell));
+                        }
+                        else if (currentCell is BrickWall brickWall && brickWall.IsDestroyed)
+                        {
+                            brickWall.BombOnCell = new Bomb();
+                            changedCells.Add((PlayerCoords, currentCell));
+                        }
                     }
-                    else if (currentCell is BrickWall brickWall && brickWall.IsDestroyed)
+                }
+
+                Action = null;
+            }
+        }
+
+        private void HandleExplosions(List<(Coordinates coords, Cell cell)> changedCells)
+        {
+            for (int y = 0; y < Field.GetLength(0); y++)
+            {
+                for (int x = 0; x < Field.GetLength(1); x++)
+                {
+                    var cell = Field[y, x];
+                    if (cell.IsAffectedByExplosion)
                     {
-                        brickWall.BombOnCell = new Bomb();
-                        changedCells.Add((PlayerCoords, currentCell));
+                        cell.IsAffectedByExplosion = false;
+                        changedCells.Add((new Coordinates(x, y), cell));
                     }
                 }
             }
 
-            Action = null;
-        }
-        
-        for (int y = 0; y < Field.GetLength(0); y++)
-        {
-            for (int x = 0; x < Field.GetLength(1); x++)
+            var explodedBombs = new List<(Coordinates coords, Bomb bomb)>();
+
+            for (int y = 0; y < Field.GetLength(0); y++)
             {
-                var cell = Field[y, x];
-                if (cell.IsAffectedByExplosion)
+                for (int x = 0; x < Field.GetLength(1); x++)
                 {
-                    cell.IsAffectedByExplosion = false;
-                    changedCells.Add((new Coordinates(x, y), cell));
+                    var cell = Field[y, x];
+                    if (cell.BombOnCell != null && cell.BombOnCell.IsExploded())
+                    {
+                        explodedBombs.Add((new Coordinates(x, y), cell.BombOnCell));
+                    }
                 }
+            }
+
+            foreach (var bombData in explodedBombs)
+            {
+                HandleBombExplosion(bombData, changedCells);
             }
         }
 
-        var explodedBombs = new List<(Coordinates coords, Bomb bomb)>();
-
-        for (int y = 0; y < Field.GetLength(0); y++)
-        {
-            for (int x = 0; x < Field.GetLength(1); x++)
-            {
-                var cell = Field[y, x];
-                if (cell.BombOnCell != null && cell.BombOnCell.IsExploded())
-                {
-                    explodedBombs.Add((new Coordinates(x, y), cell.BombOnCell));
-                }
-            }
-        }
-
-        foreach (var bombData in explodedBombs)
+        private void HandleBombExplosion((Coordinates coords, Bomb bomb) bombData, List<(Coordinates coords, Cell cell)> changedCells)
         {
             var bomb = bombData.bomb;
             for (int y = bombData.coords.Y - bomb.Radius; y <= bombData.coords.Y + bomb.Radius; y++)
@@ -255,34 +262,7 @@ public class GameProcess
                     {
                         cell.ExplodeCell(Field);
                         changedCells.Add((new Coordinates(x, y), cell));
-
-                        // Логіка обробки ворогів на клітинці
-                        var enemiesToRemove = new List<ActiveEntity>();
-                        foreach (var entity in cell.EntitiesOnCell)
-                        {
-                            GameRender.DrawLives(entity.Lives, Field.GetLength(0));
-
-                            if (entity.IsDead())
-                            {
-                                if (entity.GetEntityType() == ActiveEntityType.Player)
-                                {
-                                    IsGameOver = true;
-                                    GameRender.DrawLives(entity.Lives, Field.GetLength(0));
-                                }
-                                else if (entity.GetEntityType() == ActiveEntityType.Enemy)
-                                {
-                                    enemiesToRemove.Add(entity);
-                                    cell.PrizeOnCell = new Heart();
-
-                                    Enemies.Remove(entity as Enemy);
-                                }
-                            }
-                        }
-
-                        foreach (var enemy in enemiesToRemove)
-                        {
-                            cell.EntitiesOnCell.Remove(enemy);
-                        }
+                        HandleExplosionEffects(cell);
                     }
                 }
             }
@@ -293,15 +273,43 @@ public class GameProcess
             changedCells.Add((new Coordinates(bombData.coords.X, bombData.coords.Y), Field[bombData.coords.Y, bombData.coords.X]));
         }
 
-        GameRender.DrawChanges(changedCells);
-    }
+        private void HandleExplosionEffects(Cell cell)
+        {
+            var enemiesToRemove = new List<ActiveEntity>();
+            foreach (var entity in cell.EntitiesOnCell)
+            {
+                GameRender.DrawLives(entity.Lives, Field.GetLength(0));
 
-    private void HandleWin()
-    {
-        Win = true;
-        Timer.StopTimer();
-        Console.Clear();
-        IsGameOver = true;
-        Console.WriteLine("Congratulations! You win! Your Score: " + Score);
+                if (entity.IsDead())
+                {
+                    if (entity.GetEntityType() == ActiveEntityType.Player)
+                    {
+                        IsGameOver = true;
+                        GameRender.DrawLives(entity.Lives, Field.GetLength(0));
+                    }
+                    else if (entity.GetEntityType() == ActiveEntityType.Enemy)
+                    {
+                        enemiesToRemove.Add(entity);
+                        cell.PrizeOnCell = new Heart();
+
+                        Enemies.Remove(entity as Enemy);
+                    }
+                }
+            }
+
+            foreach (var enemy in enemiesToRemove)
+            {
+                cell.EntitiesOnCell.Remove(enemy);
+            }
+        }
+
+        private void HandleWin()
+        {
+            Win = true;
+            Timer.StopTimer();
+            Console.Clear();
+            IsGameOver = true;
+            Console.WriteLine("Congratulations! You win! Your Score: " + Score);
+        }
     }
 }
